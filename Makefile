727d90b1454d5d470e7468517d0eeb716d63c981
Aleksandre Kandelaki <aleksandre.kandelaki@tum.de>
2026-02-10 02:04:15 +0100

    add rot_utils from vggt

---
+++ Makefile
@@ -0,0 +1,21 @@
+SHELL := /bin/bash
+
+
+YELLOW := "\e[1;33m"
+NC := "\e[0m"
+
+# Logger function
+INFO := @bash -c '\
+  printf $(YELLOW); \
+  echo "=> $$1"; \
+  printf $(NC)' SOME_VALUE
+
+.venv:  # creates .venv folder if does not exist
+	python3.10 -m venv .venv
+
+
+.venv/bin/uv: .venv # installs latest pip
+	.venv/bin/pip install -U uv
+
+install: .venv/bin/uv 
+	.venv/bin/python3 -m uv pip install -r requirements.txt
---
+++ uco3d/try.py
@@ -0,0 +1,44 @@
+from uco3d import UCO3DDataset, UCO3DFrameDataBuilder
+from uco3d.dataset_utils.utils import get_dataset_root
+from uco3d import opencv_cameras_projection_from_uco3d
+import os
+# Get the dataset root folder and check that
+# all required metadata files exist.
+dataset_root = get_dataset_root(assert_exists=True)
+# Get the "small" subset list containing a small subset
+# of the uCO3D categories. For loading the whole dataset
+# use "set_lists_all-categories.sqlite".
+subset_lists_file = os.path.join(
+    dataset_root,
+    "set_lists", 
+    "set_lists_3categories-debug.sqlite",
+)
+dataset = UCO3DDataset(
+    subset_lists_file=subset_lists_file,
+    subsets=["test"],
+    frame_data_builder=UCO3DFrameDataBuilder(
+        apply_alignment=False,
+        load_images=False,
+        load_depths=False,
+        load_masks=False,
+        load_depth_masks=False,
+        load_gaussian_splats=False,
+        gaussian_splats_truncate_background=False,
+        load_point_clouds=False,
+        load_segmented_point_clouds=False,
+        load_sparse_point_clouds=False,
+        box_crop=True,
+        box_crop_context=0.4,
+        load_frames_from_videos=True,
+        image_height=800,
+        image_width=800,
+        undistort_loaded_blobs=True,
+    )
+)
+# query the dataset object to obtain a single video frame of a sequence
+
+frame_data = dataset[100]
+R, tvec, camera_matrix = opencv_cameras_projection_from_uco3d(
+    frame_data.camera,
+    image_size=frame_data.image_size_hw[None],
+)  # R, tvec, camera_matrix follow OpenCV's camera definition
